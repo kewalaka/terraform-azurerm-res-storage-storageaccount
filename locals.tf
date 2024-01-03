4f93a69c6396cd2a3983cb7a46cd6ec3eab900e6
kewalaka <kewalaka@gmail.com>
2024-01-04 09:24:57 +1300

    fix: issues reported by grept

--- locals.tf
+++ locals.tf
@@ -2,7 +2,6 @@ locals {
   blob_endpoint            = length(var.containers) == 0 ? [] : ["blob"]
   endpoints                = toset(concat(local.blob_endpoint, local.queue_endpoint, local.table_endpoint))
   private_endpoint_enabled = var.private_endpoints != null
-  private_endpoints        = local.private_endpoint_enabled ? local.endpoints : toset([])
   queue_endpoint           = length(var.queues) == 0 ? [] : ["queue"]
   table_endpoint           = length(var.tables) == 0 ? [] : ["table"]
 
